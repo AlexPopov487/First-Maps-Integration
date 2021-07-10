bd9aa12a4b4d4dd997961d63889452cb2b557ba1
apopov974@gmail.com <15LeXa97>
2021-07-11 03:45:07 +0500

    map is implemented

--- app/src/main/java/com/example/mapstest/MapFragment.kt
+++ app/src/main/java/com/example/mapstest/MapFragment.kt
@@ -9,16 +9,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.coroutineScope
+import com.example.mapstest.utils.createIcon
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.collections.MarkerManager
+import com.google.maps.android.ktx.awaitAnimateCamera
 import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.model.cameraPosition
+import com.google.maps.android.ktx.utils.collection.addMarker
 
 
 class MapFragment : Fragment() {
@@ -53,7 +58,7 @@ class MapFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val mapFragment =
-            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+            childFragmentManager.findFragmentById(R.id.main_map_fragment) as SupportMapFragment
 
         lifecycle.coroutineScope.launchWhenCreated {
             googleMap = mapFragment.awaitMap().apply {
@@ -66,28 +71,30 @@ class MapFragment : Fragment() {
                 }
             }
             when {
+                // 1. Проверяем есть ли уже права
                 ContextCompat.checkSelfPermission(
                     requireContext(),
                     Manifest.permission.ACCESS_FINE_LOCATION
                 ) == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(requireContext(), "We have permission!", Toast.LENGTH_SHORT)
+                        .show()
                     googleMap.apply {
                         isMyLocationEnabled = true
                         uiSettings.isMyLocationButtonEnabled = true
                     }
 
-                    val fusedLocationProviderClient =
-                        LocationServices.getFusedLocationProviderClient(
-                            requireActivity()
-                        )
+                    val fusedLocationProviderClient = LocationServices
+                        .getFusedLocationProviderClient(requireActivity())
+
                     fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-                        print(it)
+                        println(it)
                     }
                 }
-
+                // 2. Должны показать обоснование необходимости прав
                 shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
                     // TODO: show rationale dialog
                 }
-
+                // 3. Запрашиваем права
                 else -> {
                     requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
                 }
@@ -96,10 +103,39 @@ class MapFragment : Fragment() {
             val target = LatLng(55.751999, 37.617734)
 
 
-            val markerManager = MarkerManager(googleMap)
-            val collection = markerManager.newCollection()
 
+            val markerManager = MarkerManager(googleMap)
+            val collection = markerManager.newCollection().apply {
+                addMarker {
+                    position(target)
+                    createIcon(
+                        AppCompatResources.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_marker_foreground
+                        )!!
+                    )
+                    title("The Kremlin")
+                }.apply {
+                    tag = "Any data in tag"
+                }
+            }
+
+            collection.setOnMarkerClickListener {
+                Toast.makeText(
+                    requireContext(),
+                    "Marker \"${it.title}\" is  clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+                true
+            }
+
+            googleMap.awaitAnimateCamera(
+                CameraUpdateFactory.newCameraPosition(
+                    cameraPosition {
+                        target(target)
+                        zoom(15F)
+                    }
+                ))
         }
     }
-
 }
